ec8fda3d79df15a97a146ce3c96f46565b64eb92
ImDhairya <pandyadhairya789@gmail.com>
2025-03-20 09:27:31 +0530

    working color changer need to add some values

---
+++ Learning/Phase 2- JavaScript Surgeons/day-3ProjectAdvanceColorPicker/script.js
@@ -0,0 +1,41 @@
+const colorInput = document.getElementById("colorInput");
+const colorCode = document.getElementById("colorCode");
+const complementaryColorContainer = document.getElementById(
+  "complementaryContainer"
+);
+const copyButton = document.getElementById("copyButton");
+
+colorInput.addEventListener("change", getColorValue);
+
+function getColorValue() {
+  const currColor = colorInput.value;
+  colorCode.innerHTML = `${currColor}`;
+  getInverseColor(currColor);
+}
+
+function getInverseColor(currColor) {
+  // #d72d2d
+  const colorValue = currColor.slice(1, 7);
+  console.log(colorValue, currColor);
+
+  const redContent = colorValue.slice(0, 2);
+  const greenContent = colorValue.slice(2, 4);
+  const blueContent = colorValue.slice(4, 6);
+  // console.log(redContent, greenContent, blueContent)
+
+  // get the color in hexadecimal value and then xor it with 0xFFFFFF
+
+  const base = parseInt(String(colorValue), 16);
+  const inverseValue = 0xffffff ^ base;
+  const inverseColor = inverseValue.toString(16).padStart(6, "0");
+  // console.log("#"+inverseColor);
+  complementaryColorContainer.style.height = "100px";
+  complementaryColorContainer.style.width = "100px";
+  complementaryColorContainer.style.backgroundColor = `#${inverseColor}`;
+}
+
+function copyColor() {
+  const val = document.getElementById("colorCode");
+  navigator.clipboard.writeText(val.textContent);
+  console.log(val.textContent, "FEF")
+}
